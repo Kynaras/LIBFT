4e866cdf0fb72d34d5b30146d3efed284de9e867
Kyle EVERETT <keverett@c5r4s5.wethinkcode.co.za>
2019-05-22 16:33:52 +0200

    Donegit add *

---
+++ Functions/ft_memcmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keverett <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 15:47:59 by keverett          #+#    #+#             */
+/*   Updated: 2019/05/22 16:33:36 by keverett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t i;
+	const char *src1;
+	const char *src2;
+
+	i = 0;
+	src1 = (const char*) s1;
+	src2 = (const char*) s2;
+
+	if (n == 0)
+		return (0);
+	while (i < n)
+	{
+		if (src1[i] == src2[i])
+		{
+			i++;
+		}
+		else
+			return (src1[i] - src2[i]);
+	}
+	return (0);
+}
